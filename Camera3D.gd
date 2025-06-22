a40513c157b1471326c61b60186b8a9dd47519cf
dulbv <dusty.77.rhodes@gmail.com>
2025-06-22 13:01:25 -0700

    init

---
+++ Camera3D.gd
@@ -0,0 +1,10 @@
+extends Camera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	x.position = 
+	
+	pass # Replace with function body.
+
+
---
+++ Player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody3D
+
+@onready var sprite := $AnimatedSprite3D
+
+const SPEED = 0.75
+const JUMP_VELOCITY = 1
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	
+	if Input.is_action_pressed("walk_left"):
+		sprite.play("walk")
+		sprite.flip_h = true
+	elif Input.is_action_pressed("walk_right"):
+		sprite.play("walk")
+		sprite.flip_h = false
+	elif Input.is_action_pressed("walk_down"):
+		sprite.play("walk")
+		sprite.flip_h = false
+	elif Input.is_action_pressed("walk_up"):
+		sprite.play("walk")
+		sprite.flip_h = false
+	else:
+		sprite.play("default")
+		
+			
