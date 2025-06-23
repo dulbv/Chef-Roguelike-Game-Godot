e37cf6b0c8e71f8b7e8ff01f7c55995b77501fcc
dulbv <dusty.77.rhodes@gmail.com>
2025-06-22 19:34:24 -0700

    front entrance added

--- Area3D.gd
+++ Area3D.gd
@@ -1,8 +1,17 @@
 extends Area3D
 
 
-@onready var sprite = $Sprite3D
+# Called when a body enters the area
+func _on_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		var head_sprite = body.get_node("HeadSprite")
+		head_sprite.visible = true
+		print("entered")
+		
 
-func _on_body_entered(body):
-	print("entered")
-	pass # Replace with function body.
+# Called when a body exits the area
+func _on_body_exited(body: Node3D) -> void:
+	if body.name == "Player":
+		var head_sprite = body.get_node("HeadSprite")
+		head_sprite.visible = false
+		print("exited")
